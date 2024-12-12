226c06b4c5cdbd28edbc7ffc58f44b3ad1116948
Gastón Valdés <gastonvaldes@gmail.com>
2024-12-12 10:22:30 +0100

    feat: icons update

--- constants/Categories.ts
+++ constants/Categories.ts
@@ -8,9 +8,9 @@ const menuCategoryList = [
   },
   {
     id: 2,
-    title: 'Programa',
+    title: 'Examen',
     iconName: "check-all",
-    slug: 'programa',
+    slug: 'examen',
     selected: false,
   },
   {
